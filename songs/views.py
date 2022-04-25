1588cec3b165093cfb88bb39bb06216d7a12e0b9
michael-prye <michael.prye@gmail.com>
2022-04-25 14:01:34 -0400

    Created Post Endpoint in Song App

--- songs/views.py
+++ songs/views.py
@@ -6,12 +6,17 @@ from rest_framework import status
 from .serializers import SongSerializer
 from .models import Song
 
-@api_view(['GET'])
+@api_view(['GET','POST'])
 def all_songs(request):
     if request.method == 'GET':
         query_set = Song.objects.all()
         serializer = SongSerializer(query_set, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer=SongSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status.HTTP_201_CREATED)
 
 @api_view(['GET'])
 def single_song(request, pk):
