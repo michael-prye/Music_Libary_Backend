9bf1f991fe9a69e7ea42ec1ad0060b3042477d99
michael-prye <michael.prye@gmail.com>
2022-04-25 13:03:56 -0400

    Created Serializer for Songs App

--- songs/views.py
+++ songs/views.py
@@ -3,11 +3,15 @@ from rest_framework.decorators import api_view
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework import status
+from .serializers import SongSerializer
+from .models import Song
 
-@api_view([])
+@api_view(['GET'])
 def all_songs(request):
     if request.method == 'GET':
-        return Response(status=status.HTTP_200_OK)
+        query_set = Song.objects.all()
+        serializer = SongSerializer(query_set, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 @api_view([])
 def single_song(request, pk):
