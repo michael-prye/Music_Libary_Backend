172be10a624061ebec1150af71d2ba2b6b8aa1b2
michael-prye <michael.prye@gmail.com>
2022-04-25 17:04:56 -0400

    Added Album art to Song Model and serializer

--- songs/models.py
+++ songs/models.py
@@ -4,6 +4,7 @@ class Song(models.Model):
     title = models.CharField(max_length=255)
     artist = models.CharField(max_length=255)
     album = models.CharField(max_length=255)
+    album_art = models.CharField(max_length=300)
     release_date = models.DateField()
     genre = models.CharField(max_length=255)
     likes = models.IntegerField()
--- songs/serializers.py
+++ songs/serializers.py
@@ -4,4 +4,4 @@ from .models import Song
 class SongSerializer(serializers.ModelSerializer):
     class Meta:
         model = Song
-        fields = ['id','title','artist','album','release_date','genre','likes']
+        fields = ['id','title','artist','album','album_art','release_date','genre','likes']
