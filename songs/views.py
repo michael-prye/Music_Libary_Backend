8f955ff694a0c0a1a161c00485a27a5da357ddca
michael-prye <michael.prye@gmail.com>
2022-04-25 16:22:54 -0400

    Added likes to models in Songs App + Added Like Song endpoint to add a like to the song

--- songs/views.py
+++ songs/views.py
@@ -1,4 +1,5 @@
 from ast import Return
+from functools import partial
 import re
 from rest_framework.decorators import api_view
 from django.shortcuts import get_object_or_404
@@ -19,17 +20,26 @@ def all_songs(request):
         serializer.save()
         return Response(serializer.data, status.HTTP_201_CREATED)
 
-@api_view(['GET', 'PUT','DELETE'])
+@api_view(['GET', 'PUT','DELETE','PATCH'])
 def single_song(request, pk):
     query_set = get_object_or_404(Song, pk=pk)
     if request.method == 'GET':
         serializer = SongSerializer(query_set)
         return Response(serializer.data, status=status.HTTP_200_OK)
     elif request.method == 'PUT':
-        serializer= SongSerializer(query_set, data=request.data)
+        serializer= SongSerializer(query_set, data=request.data, partial=True)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_200_OK)
     elif request.method=='DELETE':
         query_set.delete()
         return Response(status=status.HTTP_200_OK)
+    elif request.method=='PATCH':
+        like = request.data
+        if like['likes'] == 1:
+           query_set.likes += 1
+           query_set.save()
+           serializer = SongSerializer(query_set)
+           return Response(serializer.data, status=status.HTTP_200_OK)
+            
+
