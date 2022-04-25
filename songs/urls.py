ead6ef58f8f94f2336b74b35ac6239c57ed2fbd8
michael-prye <michael.prye@gmail.com>
2022-04-25 14:18:15 -0400

    Created Delete Endpoint + Added Postman File

--- songs/urls.py
+++ songs/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.all_songs),
-    path('<int:pk>', views.single_song)
+    path('<int:pk>/', views.single_song)
     
 ]
--- songs/views.py
+++ songs/views.py
@@ -1,4 +1,5 @@
 from ast import Return
+import re
 from rest_framework.decorators import api_view
 from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
@@ -18,7 +19,7 @@ def all_songs(request):
         serializer.save()
         return Response(serializer.data, status.HTTP_201_CREATED)
 
-@api_view(['GET', 'PUT'])
+@api_view(['GET', 'PUT','DELETE'])
 def single_song(request, pk):
     query_set = get_object_or_404(Song, pk=pk)
     if request.method == 'GET':
@@ -29,3 +30,6 @@ def single_song(request, pk):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method=='DELETE':
+        query_set.delete()
+        return Response(status=status.HTTP_200_OK)
